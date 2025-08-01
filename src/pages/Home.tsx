22d7d7058a70935887a684c775db5451b693810e
Hina0222 <hina0222@naver.com>
2025-08-01 15:54:56 +0900

    style: home화면 스타일 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,45 @@
 import { Link } from 'react-router-dom';
-import Logo from '@/assets/Logo.png';
 
 export default function Home() {
   return (
     <>
-      <section className="mb-12 flex items-start justify-between">
-        <div className="flex-1">
-          <h2 className="text-orange-primary typography-SB5 inline-block rounded-full bg-white px-5 py-3">
-            <span className="font-bold">3학년 성지훈</span>{' '}
-            <span className="font-light">| 소담 다함께 돌봄센터</span>
-          </h2>
-        </div>
-        <img src={Logo} alt="Logo" className="max-h-[150px] max-w-[230px]" />
-        <div className="flex-1" />
+      <section className="mb-16">
+        <h2 className="typography-SB5 mb-6 inline-block rounded-full bg-white px-5 py-3 text-[#8B8B8B]">
+          <span className="] font-bold">3학년 성지훈</span>{' '}
+          <span className="font-light">| 소담 다함께 돌봄센터</span>
+        </h2>
+        <h1 className="font-one-mobile-pop text-orange-primary text-shadow-white-4px text-center text-[60px]">
+          이로운 한글
+        </h1>
       </section>
 
-      <section className="gap-y-7.5 grid flex-1 grid-cols-2 grid-rows-2 gap-x-10">
-        <Link
-          to={'/study'}
-          className="bg-orange-primary row-span-2 flex items-end justify-center rounded-xl px-7 py-6"
-        >
-          <h3 className="typography-SB5 text-orange-primary w-[150px] rounded-full bg-white py-3 text-center">
-            메인학습
-          </h3>
+      <section className="gap-y-7.5 grid flex-1 grid-cols-[55%_44%] grid-rows-2 gap-x-10">
+        <Link to={'/study'} className="row-span-2 flex flex-col rounded-xl bg-[#FFB14E] px-7 py-6">
+          <div className="mb-5 ml-5 mt-auto text-left">
+            <h3 className="typography-SB4 mb-1 text-white">메인학습</h3>
+            <p className="typography-R5 text-white">오늘 배울 내용, 같이 보러 가볼까요?</p>
+          </div>
+          <h4 className="typography-SB5 text-orange-primary w-full rounded-full bg-white py-3 text-center">
+            바로가기
+          </h4>
         </Link>
-        <button className="row-span-1 items-start rounded-xl bg-[#A4DF34] px-7 py-6">
-          <h3 className="typography-SB5 w-[138px] rounded-full bg-white py-3 text-center text-[#A4DF34]">
-            독서활동
-          </h3>
+        <button className="row-span-1 flex-col justify-between rounded-xl bg-[#CCE997] px-7 py-6">
+          <div className="text-left">
+            <h3 className="typography-SB4 mb-1 text-white">독서활동</h3>
+            <p className="typography-R5 text-white">나만의 도서관, 독서 통장을 쌓아요!</p>
+          </div>
+          <h4 className="typography-SB3 ml-auto w-[126px] rounded-full bg-white py-3 text-center text-[#84BC46]">
+            바로가기
+          </h4>
         </button>
-        <button className="bg-yellow-primary row-span-1 items-start rounded-xl px-7 py-6">
-          <h3 className="typography-SB5 text-yellow-primary w-[138px] rounded-full bg-white py-3 text-center">
-            맞춤법 카드
-          </h3>
+        <button className="row-span-1 flex-col justify-between rounded-xl bg-[#FFE670] px-7 py-6">
+          <div className="text-left">
+            <h3 className="typography-SB4 mb-1 text-white">맞춤법 카드 책장</h3>
+            <p className="typography-R5 text-white">학습한 맞춤법을 카드로 모아 내 책장으로!</p>
+          </div>
+          <h4 className="typography-SB3 ml-auto w-[126px] rounded-full bg-white py-3 text-center text-[#FFCE1F]">
+            바로가기
+          </h4>
         </button>
       </section>
     </>
